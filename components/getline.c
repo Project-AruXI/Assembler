a547c8f635dff47fc3b7c05ae872dcf5da61fcfa
Jorge <jorgegarcia_tx@outlook.com>
2025-12-15 18:12:04 -0600

    Include getline for windows

---
+++ components/getline.c
@@ -0,0 +1,37 @@
+#include "getline.h"
+
+ssize_t getline(char** restrict lineptr, size_t* restrict n, FILE* restrict stream) {
+  register char c;
+  register char* cs = NULL;
+  register int length = 0;
+
+  c = _getc_nolock(stream);
+  if (c == EOF) return -1;
+
+  *lineptr = (char*) malloc(64);
+  // if (!*lineptr) handleError(ERR_MEM, FATAL, "Could not allocate space for lineptr!\n");
+  *n = 64;
+
+  while (c != EOF) {
+    if (length + 1 >= *n) {
+      size_t newsize = *n + (*n >> 2);
+      if (newsize < 64) newsize = 64;
+
+      cs = (char*) realloc(*lineptr, newsize);
+      // if (!cs) handleError(ERR_MEM, FATAL, "Could not reallocate space for lineptr!\n");
+
+      *n = newsize;
+      *lineptr = cs;
+    }
+
+    ((unsigned char*) *lineptr)[length++] = c;
+
+    if (c == '\n') break;
+
+    c = _getc_nolock(stream);
+  }
+
+  (*lineptr)[length] = '\0';
+
+  return (ssize_t) length;
+}
---
+++ headers/getline.h
@@ -0,0 +1,16 @@
+#ifndef _GETLINE_H_
+#define _GETLINE_H_ 1
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifdef _WIN64
+typedef __int64   int64_t;
+#define ssize_t __int64
+#else
+#define ssize_t long
+#endif
+
+ssize_t getline(char** restrict lineptr, size_t* restrict n, FILE* restrict stream);
+
+#endif
